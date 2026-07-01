1f651a6a41e8867fd6a5e53906de6eadbc82a25e
Will Foti <wfoti71992@gmail.com>
2026-07-01 11:46:13 -0500

    Remove stray dev lookup script

--- packages/db/get-owner.mjs
+++
@@ -1,8 +0,0 @@
-import { prisma } from "./src/index.js";
-const owner = await prisma.user.findUnique({ where: { email: "owner@fieldandledger.test" }, include: { memberships: true } });
-const viewer = await prisma.user.findUnique({ where: { email: "viewer@fieldandledger.test" }, include: { memberships: true } });
-console.log("RESULT " + JSON.stringify({
-  ownerId: owner?.id, ownerFarm: owner?.memberships[0]?.farmId,
-  viewerId: viewer?.id, viewerFarm: viewer?.memberships[0]?.farmId,
-}));
-process.exit(0);
